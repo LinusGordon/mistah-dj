2b483324bc15512fbb9c0dc97c1ced120b755774
Linus Gordon <linusgordon@gmail.com>
2016-10-15 09:19:16 -0400

    bug

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 'use strict'
 
+var script = document.createElement('script');
+script.src = 'http://code.jquery.com/jquery-1.11.0.min.js';
+script.type = 'text/javascript';
+document.getElementsByTagName('head')[0].appendChild(script);
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
